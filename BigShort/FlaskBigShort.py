5d70aa5210313a2eaf523ee3156ba999fce294dc
Vittorio Niespolo <vittorio.niespolo@micegroup.it>
2024-08-05 23:11:42 +0200

    VixKong

--- BigShort/FlaskBigShort.py
+++ BigShort/FlaskBigShort.py
@@ -10,24 +10,33 @@ from ibapi.contract import Contract
 import threading
 import time
 
+flaskApp = Flask(__name__)
 
-app = Flask(__name__)
+class IBapi(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.data = [] #Initialize variable to store candle
 
-@app.route("/big-short-prediction")
+    def historicalData(self, reqId, bar):
+        print(f'DateTime: {bar.date}  Open: {bar.open}  High: {bar.high} Close: {bar.close}  Low: {bar.low}  Volume: {bar.volume}')
+        self.data.append([bar.date, bar.open,bar.high,bar.close,bar.low,bar.volume])
+
+    def cleanData(self):
+        print('Cleaning Data')
+        self.data.clear()
+
+app = IBapi()
+
+@flaskApp.route("/big-short-prediction")
 def bigShortPred():
-    return bigShortPrediction()
+    
+    connect_app(app)
+    return bigShortPrediction(app)
 
-def bigShortPrediction():
+def run_loop():
+    app.run()
 
-    app = IBapi()
-    # app.disconnect()
-    app.connect('127.0.0.1', 7497, 123)
-
-    #Start the socket in a thread
-    api_thread = threading.Thread(target=run_loop, daemon=True)
-    api_thread.start()
-    time.sleep(7)
-
+def bigShortPrediction(app):
     result_df = pd.DataFrame()
 
     # VVIX
@@ -43,7 +52,6 @@ def bigShortPrediction():
     # SPX
     spx_contract = create_contract('SPX')
     spx_data = request_historical_data(app, spx_contract)
-
     if len(spx_data) == 0:
         print('ERROR - Probabilmente ci sono problemi di connessione alla TWS')
     else:
@@ -55,45 +63,40 @@ def bigShortPrediction():
         result_df['feat_Open-PrevClose'] = np.where(spx['Open'] - spx['Close'].shift(1) > 0, 1, 0)
         result_df['Date'] = pd.to_datetime(spx['Date'])
 
-    # Print and return result
     print('Dati inseriti nel DF', result_df)
-
-    print('final DF', result_df)
-    time.sleep(3) #sleep to allow enough time for data to be returned
-    app.disconnect()
-    time.sleep(3) #sleep to allow enough time for data to be returned
-
-    # CLASSIFICATION
-    result_df = result_df.dropna()
-    clf = joblib.load("./BigShort.joblib")
-    features = [ 'feat_Open-PrevClose', 'feat_vixOpen', 'feat_vixPrevClose', 'feat_vvixOpen', 'feat_vvixPrevClose']
-
-    # TEST
-    # datas = [[date.today(), 1, 25.61000061, 25.43000031, 89.37000275, 79.58999634]] # SELL
-    # datas = [[date.today(), 0, 23.27000046, 23.79000092, 79.58999634, 79.51000214]] # BUY
-    # result_df = pd.DataFrame(datas, columns=['Date','feat_Open-PrevClose', 'feat_vixOpen', 'feat_vixPrevClose', 'feat_vvixOpen', 'feat_vvixPrevClose'])
-
-    x_live = result_df[features]
-
-    y_live_pred = clf.predict(x_live)
-
+    
+    # Run classification
+    y_live_pred = run_classification(result_df)
     print(y_live_pred)
 
-    print(result_df)
-    if(result_df['Date'].iloc[-1]==(date.today())):
-        orderType = 'HOLD' if y_live_pred==1 else 'SELL'
+    if result_df['Date'].iloc[-1] == pd.Timestamp(date.today()):
+        orderType = 'HOLD' if y_live_pred == 1 else 'SELL'
         print('Direzione Prevista:', orderType)
     else:
         orderType = 'noTrade'
         print('Dati per oggi non ancora disponibili. Ultima data disponibile:', result_df['Date'].iloc[-1].date())
-            
+    
     print('Direzione prevista per oggi:', orderType)
-        # return orderType
 
+    disconnect_app(app)
     return orderType
 
-def run_loop():
-    app.run()
+def connect_app(app):
+    app.connect('127.0.0.1', 7497, 123)
+    api_thread = threading.Thread(target=run_loop, daemon=True)
+    api_thread.start()
+    time.sleep(3)
+
+def disconnect_app(app):
+    app.disconnect()
+
+def run_classification(result_df, model_path="./BigShort.joblib"):
+    result_df = result_df.dropna()
+    clf = joblib.load(model_path)
+    features = ['feat_Open-PrevClose', 'feat_vixOpen', 'feat_vixPrevClose', 'feat_vvixOpen', 'feat_vvixPrevClose']
+    x_live = result_df[features]
+    y_live_pred = clf.predict(x_live)
+    return y_live_pred
 
 def create_contract(symbol, secType='IND', exchange='CBOE', currency='USD'):
     contract = Contract()
@@ -106,7 +109,7 @@ def create_contract(symbol, secType='IND', exchange='CBOE', currency='USD'):
 def request_historical_data(app, contract):
     app.cleanData()
     app.reqHistoricalData(1, contract, '', "2 D", "1 Day", "TRADES", 0, 1, False, [])
-    time.sleep(3)
+    time.sleep(2)
     return app.data
 
 def process_data(data, result_df, prefix):
@@ -123,16 +126,3 @@ def process_data(data, result_df, prefix):
     result_df[f'feat_{prefix}PrevClose'] = df['Close'].shift(1)
 
     return result_df
-
-class IBapi(EWrapper, EClient):
-    def __init__(self):
-        EClient.__init__(self, self)
-        self.data = [] #Initialize variable to store candle
-
-    def historicalData(self, reqId, bar):
-        print(f'DateTime: {bar.date}  Open: {bar.open}  High: {bar.high} Close: {bar.close}  Low: {bar.low}  Volume: {bar.volume}')
-        self.data.append([bar.date, bar.open,bar.high,bar.close,bar.low,bar.volume])
-
-    def cleanData(self):
-        print('Cleaning Data')
-        self.data.clear()
---
+++ VIXkONG/lib/Utility.py
@@ -0,0 +1,103 @@
+import calendar
+import time
+import pandas as pd
+import numpy as np # Fundamental package for scientific computing with Python
+import yfinance as yf #Alternative package if webreader does not work: pip install yfinance
+from datetime import date, timedelta
+from sklearn import preprocessing
+
+def exportExcelWithTimeStamp(df, prefix, postfix):
+    current_GMT = time.gmtime()
+    time_stamp = calendar.timegm(current_GMT)
+    df.to_excel(prefix+str(time_stamp) + postfix)
+
+def computeEquity(dataframe, commissioni, feats, pred_prob):
+    threshold = 0.5
+    
+    df = pd.DataFrame(dataframe)
+   
+    df['pct_change'] = (df['Close']-df['Open'])
+    # df['signal'] = np.where(df['cluster'] == 1, 1, np.where(df['cluster'] == -1, -1, 0))
+    df['commissione'] = abs(df['cluster'])*commissioni*df['Open']
+    # df['gain'] = (df['cluster']*df['pct_change'])-df['commissione']
+
+    # df['equity'] = np.cumsum(df['gain'])
+
+
+    df2 = pd.DataFrame(pred_prob)
+    df2.index = df.index
+    df = df.merge(df2, left_index=True, right_index=True)
+    #print(df)
+    df['signal'] = np.where(df[0] >= threshold, -1, np.where(df[1] >= threshold, 1, 0))
+    # df['signal'] = np.where((df[0] >= threshold) , -1, 0) #Considero solo SHORT
+    df['gain'] = (df['signal']*df['pct_change'])-df['commissione']*abs(df['signal'])
+    df['longGain'] = np.where(df['signal'] == 1,df['signal']*df['pct_change'], 0)
+    df['shortGain'] = np.where(df['signal'] == -1, df['signal']*df['pct_change'], 0)  
+    df['equity'] = np.cumsum(df['gain'])
+
+    df['HighValue'] = df['equity'].cummax()
+    df['Drawdown'] = df['equity'] - df['HighValue']
+
+    df['maxDD'] = df["Drawdown"].min()
+    equity = df["equity"].iloc[-1]
+    print('Equity:', equity)
+
+    df['anni'] = df["equity"].size/240
+    df['cagr'] = df["equity"].iloc[-1]/df["anni"].iloc[-1]
+
+    df['numOp'] = df[df["gain"] != 0].count()['gain']
+    # print('Numero di Eseguiti', numOp)
+
+    current_GMT = time.gmtime()
+    time_stamp = calendar.timegm(current_GMT)
+    df.to_excel("Equity/"+str(time_stamp) + ''.join(feats) +"ComputedEquity.xlsx")
+
+    return df
+
+def getData(start, end):
+
+    # tomorrow = "2023-09-04"
+
+    spx = yf.download (tickers = "^SPX", start = start,
+                                end = end, interval = "1d").reset_index()
+    data = yf.download (tickers = "^VIX", start = start,
+                                end = end, interval = "1d").reset_index()
+    vvix = yf.download (tickers = "^VVIX", start = start,
+                                end = end, interval = "1d").reset_index()
+    # print(data.head())
+    # print(vix.head())
+    # print(vvix.head())
+    
+    data['feat_spxGap'] = (spx['Open']-spx['Close'].shift(1))/spx['Close'].shift(1)
+    data['feat_spxPrevRange'] = (spx['High'].shift(1)-spx['Low'].shift(1))/spx['Open'].shift(1)
+    data['feat_Spx_Open-PrevClose'] = np.where(spx['Open']-spx['Close'].shift(1)>0,1,0)
+    data['feat_Open-PrevClose'] = np.where(data['Open']-data['Close'].shift(1)>0,1,0)
+    data['feat_vixOpen'] = data['Open']
+    data['feat_vixPrevClose'] = data['Close'].shift(1)
+    vvix['feat_vvixOpen'] = vvix['Open']
+    vvix['feat_vvixPrevClose'] = vvix['Close'].shift(1)
+    vvix['feat_vvixPrevOpen'] = vvix['Open'].shift(1)
+    vvix['feat_vvixPrevHigh'] = vvix['High'].shift(1)
+    vvix['feat_vvixPrevLow'] = vvix['Low'].shift(1)
+    vvix['feat_vvixPrevHigh-Low'] = (vvix['High'].shift(1)-vvix['Low'].shift(1))/vvix['Open'].shift(1)
+    # data['feat_VolChange'] = (data['Volume'].shift(1) - data['Volume'].shift(2)) / data['Volume'].shift(2)
+    # data['feat_prevClose'] = data['Close'].shift(1)
+    # data['bigBodyRange'] = np.where(abs(data['Close'] - data['Open'])/data['Open'] > 0.004, 1, 0)
+    # vix['feat_Prova'] = 1000*(vix['High'].shift(1)-vix['Low'].shift(1))/vix['Open']
+    vvix['feat_Acc'] = np.where(vvix['Close'].shift(1) > vvix['Close'].shift(2), 1, 0)
+    spx = spx.drop(['Open','Close', 'High','Low', 'Adj Close', 'Volume'], axis=1)
+    vvix = vvix.drop(['Open','Close', 'High','Low', 'Adj Close', 'Volume'], axis=1)
+    data = data.merge(spx, how='left', on='Date')# VIX
+    data = data.merge(vvix, how='left', on='Date')# vVIX
+    # data['Cluster'] = np.where((data['Close'] < data['Open']) & (abs(data['Close'] - data['Open'])/data['Open'] > bigbodyRangeValue), -1, 1)
+    data['Cluster'] = np.where(data['Close'] > data['Open'], 1, -1)
+    # data['Cluster'] = np.where(data['Close'] > data['Open']+data['Open']*0.001, 1, np.where(data['Close'] < data['Open']-data['Open']*0.001, -1,0))
+    data['feat_PrevCluster'] = data['Cluster'].astype('Int64').shift(1)
+    # data['feat_prevBigBodyRange'] = np.where(abs(data['Close'].shift(1) - data['Open'].shift(1))/data['Open'].shift(1) > bigbodyRangeValue, 1, 0)
+    # data['feat_bigBodyRange'] = np.where(abs(data['Close'] - data['Open'])/data['Open'] > 0.01, 1, 0)
+
+    # data['feat_PrevBody'] = 100000*(data['Close'].shift(1)-data['Open'].shift(10))/data['Open'].shift(10)
+
+    data =data.dropna()
+    # data.to_excel('data1.xlsx')
+    return data
